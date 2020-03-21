dfdf803eb481ea4788a70e27eb2d86a4448a8579
Kmelow <camilo.c@protonmail.com>
2020-03-21 21:35:45 +0100

    Adding db driver

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+const dotenv = require('dotenv');
 const express = require('express');
+const connectDB = require('./db/connect');
 
+// Set configurations for dotenv
+dotenv.config({ path: './config.env' });
+
+// Load routes
 const auth = require('./routes/auth');
+const users = require('./routes/users');
+const purchases = require('./routes/purchases');
+const configs = require('./routes/configs');
 
+// Launch App
 const app = express();
 
+// Connect to DB
+connectDB();
+
+// Middlewares
 app.use(express.json());
 
+// Routes
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/purchases', purchases);
+app.use('/api/v1/configs', configs);
 
 const PORT = process.env.PORT || 5000;
 
