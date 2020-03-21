2695757122e8257f767f95b5ff9a029fd3244f77
Kmelow <camilo.c@protonmail.com>
2020-03-21 22:50:56 +0100

    Added some local routes

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const users = require('./routes/users');
 const app = express();
 
 // Routes
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/', (req, res) => res.send('Got a POST request'));
+app.put('/', (req, res) => res.send('Got a PUT request at /'));
+app.delete('/', (req, res) => res.send('Got a DELETE request at /user'));
+
 app.use('/api/v1/users', users);
 
 const PORT = process.env.PORT || 5000;
